fea455a13536a82d8955e108ccba10778e58b6ff
Ibukun Adeniyi <ibkadeniyi@gmail.com>
2018-02-02 02:10:42 +0100

    added sms api

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var config = require('./server/config/config')
 var app = require('./server/server')
 var logger = require('./server/utils/logger')
-var functions = require('firebase-functions')
-exports.app=functions.https.onRequest(app)
+
 app.listen(config.port)
 logger.log("listening on http://localhost:" + config.port)
 
